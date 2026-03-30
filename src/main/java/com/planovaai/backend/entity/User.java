fb2392c74169c617202c6374c06e55c85eb66992
Umair Safdar <thedevumair@gmail.com>
2026-03-30 20:00:20 +0500

    Add user entity and repository

---
+++ src/main/java/com/planovaai/backend/entity/User.java
@@ -0,0 +1,20 @@
+package com.planovaai.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String name;
+    public String email;
+    public String password;
+}
